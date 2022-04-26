a967e21f3a38d49526c63494378c3766dcf9afd4
Marco-DElia <deliamarco99@gmail.com>
2022-04-26 17:10:44 +0200

    tests.py added, assertion proves good execution

---
+++ IPv4d2b.py
@@ -0,0 +1,47 @@
+def isIPv4(addr: str) -> bool:
+    '''
+    Description:
+        Checks if a given string is a valid IPv4 address.
+
+    Return:
+        Returns True if the address is valid, False otherwise.
+    '''
+    if type(addr) != str: return False
+    
+    bytes = addr.split(".")
+    
+    if len(bytes) != 4: return False
+    
+    for byte in bytes:
+    
+        if ( not byte.isnumeric() or\
+             not (0 <= int(byte) <= 255) ):
+           
+           return False
+           
+    return True
+ 
+ 
+def IPv4_d2b(addr: str) -> str:
+    '''         
+    Description:
+        Converts a checked valid dotted decimal IPv4 address in
+        dotted binary.
+
+    Return:
+        Returns the binary IPv4 address in a string if IPv4 address is valid, None otherwise.
+    '''
+    if not isIPv4(addr): return None
+    
+    assert(isIPv4(addr)) #can't fail
+    
+    bs = addr.split(".")
+    bs = [int(b) for b in bs]
+    
+    return f"{bs[0]:b}.{bs[1]:b}.{bs[2]:b}.{bs[3]:b}"
+
+# Usage:      
+#       
+#addr = input("Inserire un indirizzo IPv4: ")
+#print(IPv4_d2b(addr))
+#
---
+++ tests.py
@@ -0,0 +1,21 @@
+import IPv4d2b as ip
+
+def tests():
+    '''
+    Test cases:
+    '''
+    # Tests passed by execution succeded
+    print("TEST 1/8: ", ip.IPv4_d2b("255.255.255.255") == "11111111.11111111.11111111.11111111")
+    print("TEST 2/8: ", ip.IPv4_d2b("0.0.0.0") == "0.0.0.0")
+    print("TEST 3/8: ", ip.IPv4_d2b("0.255.255.0") == "0.11111111.11111111.0")
+    print("TEST 4/8: ", ip.IPv4_d2b("111.2.123.255") == "1101111.10.1111011.11111111")
+    
+    # Tests passed by execution failed
+    print("TEST 5/8: ", ip.IPv4_d2b(12345) == None)
+    print("TEST 6/8: ", ip.IPv4_d2b("1.1.1.1.1.1") == None)
+    print("TEST 7/8: ", ip.IPv4_d2b("aa.aa.aa.aa") == None)
+    print("TEST 8/8: ", ip.IPv4_d2b("256.256.256.256") == None)
+
+
+tests()
+
